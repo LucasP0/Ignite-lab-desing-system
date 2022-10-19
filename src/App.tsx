6af7d2a276c46c2132b9736439101ebebc54207e
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2022-10-19 11:18:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,12 +55,12 @@ export function App() {
       <Button type='submit' className='mt-4'> Entrar na Plataforma</Button>
       </form>
     <footer className='flex flex-col items-center gap-4 mt-8'>
-      <Text asChild size='sm'/>
+      
       <a href='' className='text-gray-400 underline hover:text-gray-200'>Esqueceu sua senha?</a>
-      <Text/>
-      <Text asChild size='sm'/>
+      
+      
       <a href=''className='text-gray-400 underline hover:text-gray-200' >Não possui conta? crie uma agora</a>
-      <Text/>
+      
 
     </footer>
 
